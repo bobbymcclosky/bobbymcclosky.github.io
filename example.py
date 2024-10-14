2829c2fb7b6849781128e3075dad9ef945d50f34
Robert McClosky <150972016+bobbymcclosky@users.noreply.github.com>
2024-10-14 06:32:36 -0400

    Delete example.py

--- example.py
+++
@@ -1,7 +0,0 @@
-
-# Example Python script that could be showcased or downloaded from the website
-def example_function():
-    print("Hello from Python script!")
-    
-if __name__ == "__main__":
-    example_function()
