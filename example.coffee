aa3560cb8b234ad4a5f34ca28f7c2dad53b69b06
Robert McClosky <150972016+bobbymcclosky@users.noreply.github.com>
2024-10-14 06:30:38 -0400

    Add files via upload

---
+++ example.coffee
@@ -0,0 +1,3 @@
+
+# Simple CoffeeScript to demonstrate functionality
+alert "This is a CoffeeScript example running in your Jekyll site"
---
+++ example.py
@@ -0,0 +1,7 @@
+
+# Example Python script that could be showcased or downloaded from the website
+def example_function():
+    print("Hello from Python script!")
+    
+if __name__ == "__main__":
+    example_function()
